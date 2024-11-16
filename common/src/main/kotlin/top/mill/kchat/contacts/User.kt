8951fbdd7fd48abf89fe8ebc141f9cb151af186f
MillHaruto <mill413@outlook.com>
2024-11-16 20:04:06 +0800

    reconstruct Contact to serialize easily

--- common/src/main/kotlin/top/mill/kchat/contacts/User.kt
+++ common/src/main/kotlin/top/mill/kchat/contacts/User.kt
@@ -6,14 +6,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class User(
     val status: UserStatus,
-    @SerialName("userID")
+    @SerialName("userUUID")
     override val id: String,
     @SerialName("userName")
     override val name: String
-) : Contact(
-    id = id,
-    name = name,
-)
+) : Contact()
 
 enum class UserStatus {
     ONLINE,
