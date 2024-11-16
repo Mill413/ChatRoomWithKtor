097f7b93fd3191240348f77195681dc1e688188a
MillHaruto <mill413@outlook.com>
2024-11-16 20:03:24 +0800

    delete unuseful schemas

--- backend/src/main/kotlin/top/mill/kchat/database/MessageSchema.kt
+++
@@ -1,7 +0,0 @@
-package top.mill.kchat.database
-
-import org.jetbrains.exposed.sql.Database
-
-class MessageSchema(database: Database) {
-    // TODO("Complete implementation of MessageSchema")
-}
--- backend/src/main/kotlin/top/mill/kchat/database/UserRoomSchema.kt
+++
@@ -1,6 +0,0 @@
-package top.mill.kchat.database
-
-
-class UserRoomSchema {
-    // TODO("Complete implementation of UserRoomSchema")
-}
