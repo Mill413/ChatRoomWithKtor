04d46df68dcebd7d669e0b3f865df8aa30ee416a
MillHaruto <mill413@outlook.com>
2024-11-16 20:03:00 +0800

    reconstruct Contact to serialize easily

--- common/src/main/kotlin/top/mill/kchat/contacts/Contact.kt
+++ common/src/main/kotlin/top/mill/kchat/contacts/Contact.kt
@@ -1,10 +1,9 @@
 package top.mill.kchat.contacts
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 
 @Serializable
-abstract class Contact(
-    @Transient open val id: String = "",
-    @Transient open val name: String = "",
-)
+sealed class Contact {
+    abstract val id: String
+    abstract val name: String
+}
