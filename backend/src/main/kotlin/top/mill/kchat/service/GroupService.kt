b9f64f559dfdca509d5d3fb8969d67904e57ce65
MillHaruto <mill413@outlook.com>
2024-11-17 23:40:16 +0800

    implement methods in GroupService

--- backend/src/main/kotlin/top/mill/kchat/service/GroupService.kt
+++ backend/src/main/kotlin/top/mill/kchat/service/GroupService.kt
@@ -1,13 +1,84 @@
 package top.mill.kchat.service
 
-// TODO("Complete implementation of ChatroomService")
+import top.mill.kchat.UUIDManager
+import top.mill.kchat.contacts.Group
+import top.mill.kchat.contacts.User
+import top.mill.kchat.database.DatabaseManager
+import top.mill.kchat.database.GroupSchema
+import top.mill.kchat.exceptions.KChatException
+import top.mill.kchat.logger
+import top.mill.kchat.network.Client
+
 class GroupService {
-    fun createRoom() {}
+    private val logger = logger("Application")
+    private val localUUID = UUIDManager.getUUIDString()
 
-    fun joinRoom() {}
+    suspend fun createGroup(group: Group): String {
+        logger.info { "Creating Group ${group.name}" }
 
-    fun leaveRoom() {}
+        onLocalUser(group.creator) {
+            Client.broadcastPostRequest(path = "group", body = group)
+        }
 
-    fun deleteRoom() {}
+        val groupSchema = GroupSchema(DatabaseManager.getDatabase())
+        if (groupSchema.getGroupByUUID(group.id) == null) {
+            return groupSchema.createGroup(group)
+        } else throw KChatException("Group ${group.id} already exists", logger)
+    }
+
+    suspend fun joinGroup(group: Group, user: User): String {
+        logger.info { "User ${user.name} joining Group ${group.name}" }
+
+        onLocalUser(user.id) {
+            Client.broadcastPutRequest(
+                uuidList = group.members.map { user -> user.id },
+                path = "group/join/${group.id}",
+                body = user
+            )
+        }
+
+        val groupSchema = GroupSchema(DatabaseManager.getDatabase())
+        if (groupSchema.getGroupByUUID(group.id) != null) {
+            return groupSchema.addUserGroup(user, group)
+        } else throw KChatException("Group ${group.id} does not exist.", logger)
+    }
+
+    suspend fun leaveGroup(group: Group, user: User): Int {
+        logger.info { "User ${user.name} leaving Group ${group.name}" }
+
+        onLocalUser(user.id) {
+            Client.broadcastPutRequest(
+                uuidList = group.members.map { user -> user.id },
+                path = "group/leave/${group.id}",
+                body = user
+            )
+        }
+
+        val groupSchema = GroupSchema(DatabaseManager.getDatabase())
+        if (groupSchema.getGroupByUUID(group.id) != null) {
+            return groupSchema.deleteUserGroup(user, group)
+        } else throw KChatException("Group ${group.id} does not exist.", logger)
+    }
+
+    suspend fun deleteGroup(group: Group): Int {
+        logger.info { "Deleting Group ${group.name}" }
+
+        onLocalUser(group.creator) {
+            Client.broadcastDeleteRequest(
+                uuidList = group.members.map { user -> user.id },
+                path = "group/${group.id}",
+                body = group
+            )
+        }
+
+        val groupSchema = GroupSchema(DatabaseManager.getDatabase())
+        if (groupSchema.getGroupByUUID(group.id) != null) {
+            return groupSchema.deleteGroupByUUID(group.id)
+        } else throw KChatException("Group ${group.id} does not exist.", logger)
+    }
+
+    private inline fun onLocalUser(uuid: String, block: () -> Unit) {
+        if (localUUID == uuid) block()
+    }
 }
 
