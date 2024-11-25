c8cf14a19ca255d6fa22ab59b1c6c018818a495e
MillHaruto <mill413@outlook.com>
2024-11-25 22:25:18 +0800

    move configure to Application.kt

--- backend/src/main/kotlin/top/mill/kchat/Application.kt
+++ backend/src/main/kotlin/top/mill/kchat/Application.kt
@@ -1,11 +1,19 @@
 package top.mill.kchat
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
 import kotlinx.coroutines.*
 import top.mill.kchat.network.Client
 import top.mill.kchat.network.broadcastDiscoveryMessage
 import top.mill.kchat.network.listenForResponses
+import top.mill.kchat.network.route.chatRoute
+import top.mill.kchat.network.route.groupsRoute
+import top.mill.kchat.network.route.usersRoute
+import kotlin.time.Duration.Companion.seconds
 
 @OptIn(DelicateCoroutinesApi::class)
 fun main(args: Array<String>) = runBlocking {
@@ -40,4 +48,24 @@ fun main(args: Array<String>) = runBlocking {
 fun Application.module() {
     configureSerialization()
     configureSockets()
+}
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
+
+fun Application.configureSockets() {
+    install(WebSockets) {
+        pingPeriod = 15.seconds
+        timeout = 15.seconds
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        chatRoute()
+        usersRoute()
+        groupsRoute()
+    }
 }
