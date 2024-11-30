7d8a71d399da4983ee2eaafc7c5f7000baa5438a
MillHaruto <mill413@outlook.com>
2024-11-30 22:06:41 +0800

    add code in ui submodule

--- ui/src/main/kotlin/Main.kt
+++ ui/src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
 package top.mill
 
-fun main() {
-    println("Hello World!")
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import top.mill.kchat.Backend
+import top.mill.kchat.UUIDManager
+import top.mill.kchat.contacts.User
+import top.mill.kchat.contacts.UserStatus
+import top.mill.kchat.service.UserService
+
+fun main(args: Array<String>): Unit = runBlocking {
+    val local = User(
+        UserStatus.ONLINE,
+        UUIDManager.getUUIDString(),
+        args[0]
+    )
+
+    launch { Backend().start(args) }
+
+    UserService().createUser(local)
 }
